86749c1e9dbf7c47b5a44dedb1a2f6b7b0d50ed8
Ler Wee Hiong <hum.ler@outlook.com>
2017-05-22 14:44:55 +0800

    Link library logo to the library homepage

--- SMU_NUI/js/custom.module.js
+++ SMU_NUI/js/custom.module.js
@@ -1,1 +1,18 @@
 var app = angular.module('viewCustom', ['angularLoad']);
+
+/**
+ * Link library logo to the library homepage.
+ */
+app.controller('prmLogoAfterController', [function () {
+  var vm = this;
+  vm.getIconLink = getIconLink;
+  function getIconLink() {
+    return vm.parentCtrl.iconLink;
+  }
+}]);
+
+app.component('prmLogoAfter', {
+  bindings: { parentCtrl: '<' },
+  controller: 'prmLogoAfterController',
+  template: '<div class="product-logo product-logo-custom" layout="row" layout-align="start center" layout-fill id="banner" tabindex="0" role="banner"><a href="https://library.smu.edu.sg"><img class="logo-image" alt="{{::(\'nui.header.LogoAlt\'|translate)}}" ng-src="{{$ctrl.getIconLink()}}" /></a></div>'
+});
