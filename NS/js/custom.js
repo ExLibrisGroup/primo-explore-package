601abeaa9e198de91998173609b617feac7cd95d
Allen Jones <ajones3066@gmail.com>
2017-06-12 17:40:56 -0400

    remove syndetics.js

--- NS/js/custom.js
+++ NS/js/custom.js
@@ -1,19 +1,20 @@
-(function () {
-    "use strict";
-    'use strict';
+(function ()
+	{
+	"use strict";
+	'use strict';
 
 
-    var app = angular.module('viewCustom', ['angularLoad']);
+	
 
-    /****************************************************************************************************/
+	/****************************************************************************************************/
 
-        /*In case of CENTRAL_PACKAGE - comment out the below line to replace the other module definition*/
+	/*In case of CENTRAL_PACKAGE - comment out the below line to replace the other module definition*/
 
-        /*var app = angular.module('centralCustom', ['angularLoad']);*/
+	/*var app = angular.module('centralCustom', ['angularLoad']);*/
 
-    /****************************************************************************************************/
+	/****************************************************************************************************/
 
-
-})();
+	
+	})();
 
    
